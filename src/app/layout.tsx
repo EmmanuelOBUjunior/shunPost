7a5650100142e43216845f6597b3659cecd1301c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-09 01:02:34 +0000

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
+import {Inter} from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className={cn('bg-white text-slate antialiased', inter.className)}>
+      <body className='min-h-screenbg-slate-50 antialiased'>
+        <Navbar/>
+        <div className='container max-w-7xl mx-auto h-full pt-24'>
+        {children}
+        </div>
+        <Toaster/>
+      </body>
     </html>
   )
 }
