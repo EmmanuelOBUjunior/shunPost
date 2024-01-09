aa266047a7f1d721f75bbe408fd2827f39c84188
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-09 01:45:10 +0000

    Updated button components with loading state

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
+import { Loader2 } from "lucide-react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
@@ -36,18 +37,23 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean,
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+  ({ className, variant, size, isLoading, children,asChild = false, ...props }, ref) => {
+    // const Comp = asChild ? Slot : "button"
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={isLoading}
         {...props}
-      />
+      >
+        {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin' /> : null}
+        {children}
+      </button>
     )
   }
 )
