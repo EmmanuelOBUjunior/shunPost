afb2c7b1800f59d8ae767a3d0064a78b19b4889f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-09 09:04:48 +0000

    Updated user model on prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -55,6 +55,8 @@ export const authOptions:NextAuthOptions = {
             username:dbUser.username
         }
        },
-       redirect('/') 
+       redirect(){
+        return '/'
+       } 
     },
 }
