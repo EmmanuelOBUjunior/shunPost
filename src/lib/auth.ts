c245269b79d1967e1fd53327fa3fca834148ffda
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-09 02:17:54 +0000

    Updated schema.prisma file with auth models

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 
 import { NextAuthOptions } from "next-auth";
 import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions:NextAuthOptions = {
     adapter: PrismaAdapter(db)
