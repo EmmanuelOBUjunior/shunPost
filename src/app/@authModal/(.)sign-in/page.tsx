b9ddd1d87c1631f78f48fb24e017d19094fc5dcd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 09:36:55 +0000

    Added intercept page

---
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface LoginInterceptProps {}
+
+const LoginIntercept:FC<LoginInterceptProps> = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default LoginIntercept
