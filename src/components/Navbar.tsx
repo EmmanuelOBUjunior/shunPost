726fbe5097a4482a77f5116848779525564aabfa
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 07:03:15 +0000

    Getting session on the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Icons } from "./Icons"
 import { buttonVariants } from './ui/Button'
 import { getAuthSession } from "@/lib/auth"
 const Navbar = async() => {
-  const session = await getAuthSession
+  const session = await getAuthSession()
   return (
     <div className="fixed top- inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
     <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -15,7 +15,7 @@ const Navbar = async() => {
 
      {/* search bar */}
 
-     {session ? (<p>You're logged in</p>) : (<Link href='/sign-in' className={buttonVariants()}>Sign In</Link>)}
+     {session ? (<p>Youre logged in</p>) : (<Link href='/sign-in' className={buttonVariants()}>Sign In</Link>)}
     </div>
     </div>
   )
