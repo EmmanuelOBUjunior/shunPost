166a2d4a312266a0e2fe38979027ef5099fd2556
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 09:42:05 +0000

    Working on login intercept page

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -3,9 +3,8 @@ import { FC } from 'react'
 interface LoginInterceptProps {}
 
 const LoginIntercept:FC<LoginInterceptProps> = ({}) => {
-  return (
-    <div>Intercept</div>
-  )
+  return <div>Intercept</div>
+  
 }
 
 export default LoginIntercept
