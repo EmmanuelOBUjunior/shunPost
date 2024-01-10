46eb5f176871a011d3dcb39e915aa5b1900c42cc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 08:00:00 +0000

    Added image domain to next.config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['uploadthing.com', 'lh3.googleusercontent.com']
+        // domains: ['uploadthing.com', 'lh3.googleusercontent.com']
+        remotePatterns:[{
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            port: '',
+            pathname:''
+        }]
     },
 }
 
