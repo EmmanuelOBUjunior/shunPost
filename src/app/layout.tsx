32a2a172b2293c0b16054d6ba910b52e6fbe7224
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 09:34:11 +0000

    Working on parallel routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  authModal
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html lang='en' className={cn('bg-white text-slate antialiased', inter.className)}>
       <body className='min-h-screen bg-slate-50 antialiased'>
         <Navbar/>
+
+        {authModal}
         <div className='container max-w-7xl mx-auto h-full pt-24'>
         {children}
         </div>
