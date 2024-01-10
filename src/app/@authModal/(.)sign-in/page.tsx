be6f4b259c0859335dfdbf703afa1306a5a1cc50
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 23:46:25 +0000

    updated auth file with next auth secret

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -3,8 +3,9 @@ import { FC } from 'react'
 interface LoginInterceptProps {}
 
 const LoginIntercept:FC<LoginInterceptProps> = ({}) => {
-  return <div>Intercept</div>
-  
+  return (
+  <div className='h-screen flex items-center justify-center text-zinc-950'>Intercept</div>
+  )
 }
 
 export default LoginIntercept
