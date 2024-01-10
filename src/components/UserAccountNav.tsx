102bd4fbea9292db5f8a284ca379bd5cbc37982d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 07:14:44 +0000

    Added dropdown

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react"
+import {User} from 'next-auth'
 
-interface UserAccountNavProps{}
+interface UserAccountNavProps{
+    user: Pick<User, 'name' | 'email' | 'image'>
+}
 
-const UserAccountNav: FC<UserAccountNavProps> = ({}) => {
+const UserAccountNav: FC<UserAccountNavProps> = ({user}) => {
   return (
     <div>UserAccountNav</div>
   )
