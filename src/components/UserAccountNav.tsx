680db3d85cd4e694376f0c36d694efe84d904ba4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-10 08:43:59 +0000

    Working on User Nav Bar

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react"
 import {User} from 'next-auth'
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/DropdownMenu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/DropdownMenu"
 import UserAvatar from "./UserAvatar"
+import Link from "next/link"
+import {signOut} from "next-auth'
 
 interface UserAccountNavProps{
     user: Pick<User, 'name' | 'email' | 'image'>
@@ -14,6 +16,39 @@ const UserAccountNav: FC<UserAccountNavProps> = ({user}) => {
             <DropdownMenuTrigger>
                 <UserAvatar user={user}/>
             </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <div className="flex items-center justify-start gap-2 p-2">
+                <div className="flex flex-col space-y-1 leading-none"
+                >
+                {user.name && <p className="font-medium">{user.name}</p>}
+                {user.email && <p className="w-[200px] trancate text-[0.7rem] text-zinc-700">{user.email}</p>}
+                </div>
+                </div>
+
+            <DropdownMenuSeparator/>
+
+            <DropdownMenuItem asChild>
+                <Link href='/'>Feed</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+                <Link href='/r/create'>Create Community</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+                <Link href='/settings'>Settings</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator/>
+            
+            <DropdownMenuItem className="cursor-pointer" onSelect={(e)=>{
+                e.preventDefault()
+                signOut({
+                    
+                })
+            }}>
+                Sign Out
+            </DropdownMenuItem>
+
+            </DropdownMenuContent>
         </DropdownMenu>
     </div>
   )
