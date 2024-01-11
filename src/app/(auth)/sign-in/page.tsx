ccd16b08a6509e2e255b3cd1b5a15a4982f60eff
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-11 08:39:12 +0000

    Working on close modal

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -9,7 +9,7 @@ const SignInPage = () => {
   return (
     <div className='absolute inset-0'>
         <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
-        <Link href='/' className= {cn(buttonVariants({variant: 'ghost'}), 'self-start mt-20')}><ChevronLeftIcon/>Home</Link>
+        <Link href='/' className= {cn(buttonVariants({variant: 'ghost'}), 'self-start mt-20')}><ChevronLeftIcon className='mr-2 w-4 h-4'/>Home</Link>
         <SignIn/>
         </div>
     </div>
