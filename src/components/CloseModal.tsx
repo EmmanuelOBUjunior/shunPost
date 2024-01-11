816fba2d16a58592e664541e6c978157f71595d7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-11 07:59:53 +0000

    Working on close modal

--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { X } from "lucide-react"
+import { Button } from "./ui/Button"
 
 const CloseModal = () => {
   return (
-    <div>CloseModal</div>
+    <Button variant='subtle' className="h-6 w-6 rounded-md" aria-label="close-modal">
+        <X className="h-4 w-4"/>
+    </Button>
   )
 }
 
