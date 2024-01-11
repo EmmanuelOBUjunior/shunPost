4ebb853fcfdd170d8140518ee045f052eca5444b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-11 08:14:55 +0000

    Working on intercept page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from '@/components/SignIn'
 import { buttonVariants } from '@/components/ui/Button'
 import { cn } from '@/lib/utils'
+import { ChevronLeftIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -8,7 +9,7 @@ const SignInPage = () => {
   return (
     <div className='absolute inset-0'>
         <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
-        <Link href='/' className= {cn(buttonVariants({variant: 'ghost'}), 'self-start mt-20')}>Home</Link>
+        <Link href='/' className= {cn(buttonVariants({variant: 'ghost'}), 'self-start mt-20')}><ChevronLeftIcon/>Home</Link>
         <SignIn/>
         </div>
     </div>
