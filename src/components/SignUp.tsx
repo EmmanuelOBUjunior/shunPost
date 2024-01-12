ea53632c3ce329d29fc2519ba2b2dde21b157e19
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-12 08:03:56 +0000

    Working on sign up component

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,7 +3,7 @@ import { Icons } from './Icons'
 import Link from 'next/link'
 import UserAuthForm from './UserAuthForm'
 
-const SignIn = () => {
+const SignUp = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
     <div className="flex flex-col space-y-2 text-center">
@@ -20,4 +20,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
