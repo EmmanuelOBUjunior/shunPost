b752b8dc3f0d48f3d2f9b79c59c9bf6a9ccecdba
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-12 08:03:03 +0000

    Added sign in component

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import SignIn from '@/components/SignIn'
+import SignUp from '@/components/SignUp'
 import { buttonVariants } from '@/components/ui/Button'
 import { cn } from '@/lib/utils'
 import { ChevronLeftIcon } from 'lucide-react'
@@ -10,7 +10,7 @@ const SignInPage = () => {
     <div className='absolute inset-0'>
         <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
         <Link href='/' className= {cn(buttonVariants({variant: 'ghost'}), 'self-start mt-20')}><ChevronLeftIcon className='mr-2 w-4 h-4'/>Home</Link>
-        <SignIn/>
+        <SignUp/>
         </div>
     </div>
   )
