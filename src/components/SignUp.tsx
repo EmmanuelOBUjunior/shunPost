643c097e4b888307b5b7dabe64847fcdc2b58aa0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-12 08:04:37 +0000

    Working on sign up component

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -8,13 +8,13 @@ const SignUp = () => {
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
     <div className="flex flex-col space-y-2 text-center">
     <Icons.logo className='mx-auto h-6 w-6'/>
-    <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+    <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
     <p className="text-sm max-w-xs mx-auto">By continuing, you are setting up a ShunPost account and agree to our User Agreement and Privacy Policy</p>  
     {/* Sign In Form   */}
     <UserAuthForm />
 
-    <p className='px-8 text-center text-sm text-zinc-700'>New to ShunPost? {' '}
-    <Link href='/sign-up' className='hove:text-zinc-800 text-sm underline underline-offset-4'>Sign Up</Link></p>
+    <p className='px-8 text-center text-sm text-zinc-700'>Already a ShunPoster? {' '}
+    <Link href='/sign-in' className='hove:text-zinc-800 text-sm underline underline-offset-4'>Sign In</Link></p>
     </div>
     </div>
   )
