c095d9b042b95de9fb0f04b04caa8240121e50c2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 17:33:46 +0000

    Working on Providers

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,9 +2,9 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-const Providers = ({children}:{children:React.ReactNode}) => {
+const Provider = ({children}:{children:React.ReactNode}) => {
     const queryClient = new QueryClient()
     return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
 
-export default Providers
+export default Provider
