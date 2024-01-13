208d2d2de85f8933b523f16a8863e389590fbc25
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 17:16:02 +0000

    Working on subreddit validator

--- src/lib/validator/subreddit.ts
+++ src/lib/validator/subreddit.ts
@@ -11,3 +11,5 @@ export const SubredditSubscriptionValidator = z.object({
 
 export type CreateSubredditPayload = z.infer<typeof SubredditValidator>
 
+export type SubscribeToSubredditPayload = z.infer<typeof SubredditSubscriptionValidator>
+
