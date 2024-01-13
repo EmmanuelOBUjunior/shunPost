800bd4bb789362dc74a402be7e9e7ea5221df8e6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 08:46:30 +0000

    Working on create community page

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -4,7 +4,25 @@ import { useState } from "react"
 const Create = () => {
     const [input, setInput] = useState<string>('')
   return (
-    <div>Create Community</div>
+    <div className="container flex items-center h-full max-3xl mx-auto">
+    <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
+        <div className="flex justify-between items-center">
+            <h1 className="text-xl font-semibold">Create Community</h1>
+        </div>
+        <hr className="bg-zinc-500 h-px"/>
+
+        <div>
+            <p className="text-lg font-medium">Name</p>
+            <p className="text-xs pb-2">Community names including capitalization cannot be changed</p>
+
+            <div className="relative">
+                <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
+                    r/
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
   )
 }
 
