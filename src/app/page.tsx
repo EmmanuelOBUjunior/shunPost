99c685804fafc1248d3bae9b5db1704f073583f3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 07:50:36 +0000

    Working on the subrredit info session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+import { HomeIcon } from "lucide-react";
 
 
 export default function Home() {
   return (
     <>
-    <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
+    <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
       {/* Feed */}
 
       {/* Subreddit Info */}
-      <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last"></div>
+      <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+        <div className="bg-emerald-100 px-6 py-4">
+          <p className="font-semibold py-3 flex items-center gap-1.5">
+            <HomeIcon className="w-4 h-4"/> Home
+          </p>
+        </div>
+        <div className="-my-3 divide-y diide-gray-100 px-6 py-4 text-sm leading-6">
+          <div className="flex justify-between gap-x-4 py-3">
+            <div className="text-zinc-500">
+              Your personal ShunPost homepage. Come here to check
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
