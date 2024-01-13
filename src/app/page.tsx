0ef7d9067532fe3c4c7dfdff115ee11c6615e39d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 07:55:19 +0000

    Working on the subrredit info session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { buttonVariants } from "@/components/ui/Button";
 import { HomeIcon } from "lucide-react";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -17,10 +19,14 @@ export default function Home() {
         </div>
         <div className="-my-3 divide-y diide-gray-100 px-6 py-4 text-sm leading-6">
           <div className="flex justify-between gap-x-4 py-3">
-            <div className="text-zinc-500">
+            <p className="text-zinc-500">
               Your personal ShunPost homepage. Come here to check in your favorite communities.
-            </div>
+            </p>
           </div>
+
+          <Link className={buttonVariants({
+            className: 'w-full mt-4 md-6',
+          })} href='/r/create'>Create Community</Link>
         </div>
       </div>
     </div>
