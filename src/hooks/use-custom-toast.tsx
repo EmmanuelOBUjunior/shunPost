27c197ba3a722df922049a7a2a6ff836dd43f2db
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 20:01:16 +0000

    Workin on custom toast

--- src/hooks/use-custom-toast.tsx
+++ src/hooks/use-custom-toast.tsx
@@ -1,18 +1,26 @@
-import Link from "next/link"
-import { toast } from "./use-toast"
-import { buttonVariants } from "@/components/ui/Button"
+import Link from "next/link";
+import { toast } from "./use-toast";
+import { buttonVariants } from "@/components/ui/Button";
 
-export const useCustomToast = ()=>{
-    const loginToast = () =>{
-        const {dismiss} = toast({
-            title: "Login required.",
-            description: "You need to be authorized to perform this action",
-            variant: 'destructive',
-            action:(
-                <Link href='/sign-in' className={buttonVariants({variant: 'outline'})}>Login</Link>
-            )
-        })
-    }
+export const useCustomToast = () => {
+  const loginToast = () => {
+    const { dismiss } = toast({
+      title: "Login required.",
+      description: "You need to be authorized to perform this action",
+      variant: "destructive",
+      action: (
+        <Link
+          href="/sign-in"
+          className={buttonVariants({ variant: "outline" })}
+          onClick={() => {
+            dismiss();
+          }}
+        >
+          Login
+        </Link>
+      ),
+    });
+  };
 
-    return {loginToast}
-}
+  return { loginToast };
+};
