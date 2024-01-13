fa83d1bc968fac6b495c482003a9050f8e0055cd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 18:34:58 +0000

    Working on create community api

---
+++ src/app/api/auth/[...nextauth]/subreddit/route.ts
@@ -0,0 +1,27 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SubredditValidator } from "@/lib/validator/subreddit";
+
+export async function POST(req:Request){
+    try {
+        const session = await getAuthSession()
+
+        if(!session?.user){
+            return new Response('Unauthorized', {status:401})
+        }
+        const body = await req.json()
+        const {name} = SubredditValidator.parse(body)
+        const subredditExists = await db.subreddit.findFirst({
+            where:{
+                name,
+            },
+        })
+
+        if(subredditExists){
+            return new Response('Subreddit already exists', {status:409})
+        }
+
+    } catch (error) {
+        
+    }
+}
