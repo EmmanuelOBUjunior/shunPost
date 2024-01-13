69aa29e1333ee32eac07aded9dbcb1f085603451
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-13 19:56:11 +0000

    Workin on custom toast

--- src/hooks/use-custom-toast.tsx
+++ src/hooks/use-custom-toast.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link"
+import { toast } from "./use-toast"
+import { buttonVariants } from "@/components/ui/Button"
+
 export const useCustomToast = ()=>{
-    
+    const loginToast = () =>{
+        const {dismiss} = toast({
+            title: "Login required.",
+            description: "You need to be authorized to perform this action",
+            variant: 'destructive',
+            action:(
+                <Link className={buttonVariants({variant="outline"})}Login</Link>
+            )
+        })
+    }
+
+    return {loginToast}
 }
