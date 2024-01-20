df50cb528b906e3d72e89d825a74cdbd5b20283d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-20 22:57:25 +0000

    Working on dynamic pages

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+interface PageProps{
+  params:{
+  slug: string
+}
+}
 
-const page = () => {
+
+const page = ({params}:PageProps) => {
   return (
     <div>page</div>
   )
