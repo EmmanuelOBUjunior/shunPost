afaefe78941043b2e178cb08132320115a2d2d9c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-20 23:58:08 +0000

    Working on createminipost component

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -3,6 +3,7 @@
 import { Session } from "next-auth"
 import { usePathname, useRouter } from "next/navigation"
 import { FC } from "react"
+import UserAvatar from "./UserAvatar"
 
 interface MiniCreatePostProps{
     session: Session
@@ -15,7 +16,14 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({session}) => {
     <li className="overflow-hidden rounded-md bg-white shadow">
         <div className="h-full px-6 py-4 sm:flex sm:justify-between gap-6">
             <div className="relative">
-                
+                <UserAvatar
+                user={
+                    {
+                        name: session?.name || null,
+                        image: session?.user.image || null
+                    }
+                }
+                />
             </div>
         </div>
     </li>
