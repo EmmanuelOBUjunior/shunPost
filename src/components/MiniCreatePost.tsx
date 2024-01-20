485a45e81e74755683d9ee1bd4dd1fa3dc3cccf3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-20 23:54:15 +0000

    Working on createminipost component

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,14 +1,20 @@
 'use client'
 
+import { Session } from "next-auth"
+import { usePathname, useRouter } from "next/navigation"
 import { FC } from "react"
 
 interface MiniCreatePostProps{
-
+    session: Session
 }
 
-const MiniCreatePost: FC<MiniCreatePostProps> = ({}) => {
+const MiniCreatePost: FC<MiniCreatePostProps> = ({session}) => {
+    const router = useRouter()
+    const pathName = usePathname()
   return (
-    <div></div>
+    <li className="overflow-hidden rounded-md bg-white shadow">
+
+    </li>
   )
 }
 
