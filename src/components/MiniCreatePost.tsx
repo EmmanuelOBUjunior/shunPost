df4c34e8039822daf8c492f66e2a99e166fb0076
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-21 00:03:52 +0000

    Working on createminipost component

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -14,7 +14,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({session}) => {
     const pathName = usePathname()
   return (
     <li className="overflow-hidden rounded-md bg-white shadow">
-        <div className="h-full px-6 py-4 sm:flex sm:justify-between gap-6">
+        <div className="h-full px-6 py-4 flex justify-between gap-6">
             <div className="relative">
                 <UserAvatar
                 user={
@@ -24,6 +24,8 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({session}) => {
                     }
                 }
                 />
+
+                <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white"/>
             </div>
         </div>
     </li>
