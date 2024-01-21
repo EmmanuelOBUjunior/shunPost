1a7253235a13e04ddc51a79d199e42ec1c8bc4e5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-21 00:07:17 +0000

    Working on createminipost component

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,35 +1,37 @@
-'use client'
+"use client";
 
-import { Session } from "next-auth"
-import { usePathname, useRouter } from "next/navigation"
-import { FC } from "react"
-import UserAvatar from "./UserAvatar"
+import { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
+import { FC } from "react";
+import UserAvatar from "./UserAvatar";
+import { Input } from "./ui/Input";
 
-interface MiniCreatePostProps{
-    session: Session
+interface MiniCreatePostProps {
+  session: Session;
 }
 
-const MiniCreatePost: FC<MiniCreatePostProps> = ({session}) => {
-    const router = useRouter()
-    const pathName = usePathname()
+const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
+  const router = useRouter();
+  const pathName = usePathname();
   return (
     <li className="overflow-hidden rounded-md bg-white shadow">
-        <div className="h-full px-6 py-4 flex justify-between gap-6">
-            <div className="relative">
-                <UserAvatar
-                user={
-                    {
-                        name: session?.name || null,
-                        image: session?.image || null
-                    }
-                }
-                />
-
-                <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white"/>
-            </div>
+      <div className="h-full px-6 py-4 flex justify-between gap-6">
+        <div className="relative">
+          <UserAvatar
+            user={{
+              name: session?.name || null,
+              image: session?.image || null,
+            }}
+          />
+          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
         </div>
+        <Input
+            readOnly
+            onClick={()=> router.push(pathName + '/submit')}
+        />
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default MiniCreatePost
+export default MiniCreatePost;
