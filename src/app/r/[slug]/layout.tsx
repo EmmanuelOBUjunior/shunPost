7555c68f34ba83ef4ff6683ac0b6481500bd2d16
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-21 16:07:03 +0000

    working on layout for the dynamic page

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -3,7 +3,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
         <div>
         {/* TODO: Button to take us back */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-yy-4 md:gap-x-4 py-6">
-        <div className="flex flex-col col-sapn-2 spave-y-6">{children}</div>
+        <div className="flex flex-col col-span-2 space-y-6">{children}</div>
         </div>
         </div>
         </div>
