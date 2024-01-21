c9e7bb850f702afbd4991b9204464cec2666b745
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-21 16:05:13 +0000

    working on layout for the dynamic page

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,8 @@
 const Layout = ({children}: {children: React.ReactNode}) => {
-    return <div className="sm:container max-w-7xl mx-auto h-full pt-12">{children}</div>
+    return <div className="sm:container max-w-7xl mx-auto h-full pt-12">
+        <div>
+        
+        </div>
+        </div>
 }
 export default Layout;
