0ba2b01770f43171df96ce61460ea9ae8335f452
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-22 23:04:16 +0000

    Working on subscribe to subreddit api

--- src/app/api/subreddit/subscribe/route.ts
+++ src/app/api/subreddit/subscribe/route.ts
@@ -1,4 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SubredditSubscriptionValidator } from "@/lib/validator/subreddit";
 
 export async function POST(req: Request) {
   try {
@@ -6,5 +8,11 @@ export async function POST(req: Request) {
     if (!session?.user) return new Response("Unauthorized", {status: 401});
 
     const body = await req.json()
+    const {subredditId} = SubredditSubscriptionValidator.parse(body)
+    
+    const subscriptionExists = await db.subscription.findFirst({
+        
+    })
+    
   } catch (error) {}
 }
